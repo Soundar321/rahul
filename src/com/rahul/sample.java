ede97f4d460e750ceaeced8964d094346025ba1b
Soundar321 <soundarrajan.mani007@gmail.com>
2022-09-23 14:37:46 +0530

    rahul commit

--- src/com/rahul/sample.java
+++ src/com/rahul/sample.java
@@ -4,5 +4,8 @@ public class sample {
 public static void main(String[] args) {
 	System.out.println("rahul");
 	System.out.println("arun");
+	System.out.println("rahul");
+	System.out.println("arun");
+}
 }
 }
